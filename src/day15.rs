28733bda39e141a91e0e76d250e079e871771e4d
Ben Holmes <7benholmes@gmail.com>
2021-12-17 16:52:59 -0700

    Day 15

--- src/day15.rs
+++ src/day15.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 use std::collections::{HashSet, VecDeque};
 
 const INPUT: &'static str = include_str!("../inputs/day15.txt");
@@ -13,44 +15,104 @@ fn parse1(input: &str) -> u32 {
         .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
 
-    let n_rows = grid.len();
-    let n_cols = grid[0].len();
+    shortest_path(grid).expect("No path")
+}
 
-    let mut costs = vec![vec![u32::MAX; n_cols]; n_rows];
-    costs[n_rows - 1][n_cols - 1] = grid[n_rows - 1][n_cols - 1];
+fn parse2(input: &str) -> u32 {
+    let small_grid: Vec<Vec<u32>> = input
+        .lines()
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect();
 
-    let mut queue: VecDeque<(usize, usize)> = VecDeque::from(vec![(n_rows - 1, n_cols - 1)]);
-    let mut in_queue: HashSet<(usize, usize)> = HashSet::new();
+    let n_rows = small_grid.len();
+    let n_cols = small_grid[0].len();
 
-    while !queue.is_empty() {
-        let (row, col) = queue.pop_front().unwrap();
-        if col != 0 {
-            let current_cost = costs[row][col - 1];
-            let new_cost = costs[row][col] + grid[row][col - 1];
-            costs[row][col - 1] = current_cost.min(new_cost);
-            let new_pt = ((row, col - 1));
-            if !in_queue.contains(&new_pt) {
-                queue.push_back(new_pt);
-                in_queue.insert(new_pt);
+    let mut grid = vec![vec![0; 5 * n_cols]; 5 * n_rows];
+    for row in 0..5 * n_rows {
+        for col in 0..5 * n_cols {
+            let inc = (row / n_rows) + (col / n_cols);
+            let mut val = small_grid[row % n_rows][col % n_cols] + inc as u32;
+            if val > 9 {
+                val -= 9;
             }
+            grid[row][col] = val;
         }
-        if row != 0 {
-            let current_cost = costs[row - 1][col];
-            let new_cost = costs[row][col] + grid[row - 1][col];
-            costs[row - 1][col] = current_cost.min(new_cost);
-            let new_pt = ((row - 1, col));
-            if !in_queue.contains(&new_pt) {
-                queue.push_back(new_pt);
-                in_queue.insert(new_pt);
+    }
+
+    shortest_path(grid).expect("no path")
+}
+
+fn shortest_path(grid: Vec<Vec<u32>>) -> Option<u32> {
+    let max_row = grid.len() - 1;
+    let max_col = grid[0].len() - 1;
+    let goal = (max_row, max_col);
+
+    let mut costs = vec![vec![u32::MAX; max_row + 1]; max_col + 1];
+
+    let mut heap = BinaryHeap::new();
+
+    costs[0][0] = 0;
+    heap.push(State {
+        cost: 0,
+        pos: (0, 0),
+    });
+
+    while let Some(State { cost, pos }) = heap.pop() {
+        // If we are at the goal, return
+        if pos == goal {
+            return Some(cost);
+        }
+
+        let (row, col) = pos;
+        // Ignore if we've alread found a better way. This is because we don't update the costs in the heap
+        if cost > costs[row][col] {
+            continue;
+        }
+
+        for (next_row, next_col) in [
+            (row.checked_sub(1).unwrap_or(0), col),
+            (row + 1, col),
+            (row, col.checked_sub(1).unwrap_or(0)),
+            (row, col + 1),
+        ] {
+            if (next_row, next_col) == pos || next_row > max_row || next_col > max_col {
+                continue;
+            }
+
+            let next_cost = cost + grid[next_row][next_col];
+
+            if next_cost < costs[next_row][next_col] {
+                heap.push(State {
+                    cost: next_cost,
+                    pos: (next_row, next_col),
+                });
+                costs[next_row][next_col] = next_cost;
             }
         }
     }
 
-    costs[0][0] - grid[0][0]
+    None
 }
 
-fn parse2(input: &str) -> usize {
-    0
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: u32,
+    pos: (usize, usize),
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.pos.cmp(&other.pos))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 #[cfg(test)]
@@ -75,6 +137,6 @@ mod test {
 
     #[test]
     fn second() {
-        assert_eq!(parse2(INPUTS), 0);
+        assert_eq!(parse2(INPUTS), 315);
     }
 }
